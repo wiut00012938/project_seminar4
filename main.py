1eec4ba0fef28c063781ddeb83741ca6cfebb10b
wiut00012938 <113694566+wiut00012938@users.noreply.github.com>
2022-10-05 10:47:00 +0500

    while looping with conditional statemetns

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def encode(message):
-encoded_message = ""
-i = 0
-
-
-
-# Press the green button in the gutter to run the script.
-while (i <= len(message) - 1):
-count = 1
-char = message[i_of_external]
-j = i
-while (j < len(message) - 1):
-if (message[j] == message[j + 1]):
-count = count + 1
-j = j + 1
-else:
-break
-encoded_message = encoded_message + str(count) + ch
-i = j + 1
-return encoded_message
-encoded_message = encode("ABBBBCCCCCCCCAB")
-print(encoded_message)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while True:
+    number = int(input("Enter a number: "))
+    if number % 2 == 0:
+        print("This is an even number")
+    else:
+        print("This is an odd number")
+    cont = input("Do you want to continue? ")
+    while cont .lower() not in ("yes", "no"):
+        cont = input("Do you want to continue? ")
+    if cont == "no":
+        print("Break")
+        break
